4db8ebc4e81773d8b63e22b99f57cc277eb81536
Arda Günal <102471766+Jumpingquasar@users.noreply.github.com>
2024-04-01 23:32:17 +0300

    Add Logout Button

--- src/screens/client/profile/index.tsx
+++ src/screens/client/profile/index.tsx
@@ -1,11 +1,29 @@
-import { Text, View } from "react-native"
+import { Text, TouchableOpacity, View } from "react-native"
+import textStyles from "../../../constants/textStyles"
+import { aspectratio } from "../../../constants/distances"
+import colors from "../../../constants/colors"
+import { useDispatch } from "react-redux"
+import { setUser } from "../../../store/authSlice"
 
 
 export const Profile = () => {  //Not Implemented.
-
+    const dispatch = useDispatch();
     return(
         <View style={{alignItems: 'center', justifyContent: 'center', flex: 1}}>
-            <Text>Profile Page</Text> 
+            <TouchableOpacity 
+                onPress={() => dispatch(setUser({Email: '', encryptedFormData: '', isGuest: true}))}
+                style={{
+                    width:aspectratio(100, 'width'), 
+                    height:aspectratio(40, 'height'), 
+                    alignItems: 'center', 
+                    justifyContent: 'center', 
+                    borderRadius: 10, 
+                    backgroundColor: colors.dividerGray,
+                    elevation: 25,
+                    borderWidth: 1
+                }}>
+                <Text style={textStyles.titleMedium}>Log out</Text>
+            </TouchableOpacity>
         </View>
     )
 }
